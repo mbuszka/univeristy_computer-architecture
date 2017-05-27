640703717a5d3c1d2df2195e6a870d108659b45d
Maciej Buszka <maciej@buszka.eu>
2017-05-27 18:14:02 +0200

    added raport.md

--- list-b/cache.c
+++ list-b/cache.c
@@ -8,10 +8,6 @@
 #include "common.h"
 
 #define DEBUG 0
-#define VERSION 'c'
-#define L1 (1 << 13)
-#define L2 (1 << 16)
-
 
 /* Do not touch this procedure! */
 int array_walk(volatile int *array, int steps) {
@@ -31,47 +27,41 @@ int array_walk(volatile int *array, int steps) {
 }
 
 /* XXX: You are only allowed to change this procedure! */
-void generate_permutation(int *array, int size, int perm) {
-  /* Walk over every even element, then over every odd. */
-  //for (int i = 0; i < size - 1; i += 2) {
-  //  array[i] = i + 2;
-  //  array[i + 1] = i + 3;
-  //}
-
-  /* Link the end of even list to the beginning of odd list. */
-  //array[size - 2] = 1;
-
-  /* Finish the sequence, note it's not circular! */
-  //array[size - 1] = -1;
-  
-  int s = 64;
-  int a = 18;
-  int b = L1;
+void generate_permutation(int *array, int size, int perm, int a) {
+ 
+  int d = 1 << a;
   switch (perm) {
     case 'a':
-      array[0] = a;
-      if (size < a) {
-        array[0] = 0;
-	break;
-      }
-      array[a] = 0;
-      for (int i=s; i < size - a; i += s) {
-        array[i - s + a] = i;
-        array[i] = i + a;
-        array[i + a] = 0;
+      array[0] = d;
+      for (int i = d; i < size; i+= d) {
+        array[i - d] = i;
+        array[i] = 0;
       }
       break;
     case 'b':
+      /* Walk over every even element, then over every odd. */
+      /*for (int i = 0; i < size - 1; i += 2) {
+        array[i] = i + 2;
+        array[i + 1] = i + 3;
+      }*/
+
+      /* Link the end of even list to the beginning of odd list. */
+      //array[size - 2] = 1;
+
+      /* Finish the sequence, note it's not circular! */
+      //array[size - 1] = 0;
+      d = a == 0 ? size : size/2 + d;
       array[0] = 0;
-      for (int i=16; i < size; i += 16) {
+      for (int i=16; i < d; i += 16) {
         array[i-16] = i;
         array[i] = 0;
       }
       break;
     case 'c':
+      d = 6144 << 10;
       array[0] = 0;
-      for (int i=b; i < size; i += b) {
-        array[i-b] = i;
+      for (int i=d; i < size; i += d) {
+        array[i-d] = i;
         array[i] = 0;
       }
       break;
@@ -79,7 +69,7 @@ void generate_permutation(int *array, int size, int perm) {
 }
 
 int main(int argc, char **argv) {
-  int opt, size = -1, steps = -1, times = -1, perm = -1;
+  int opt, size = -1, steps = -1, times = -1, perm = -1, a = -1;
   bool error = false;
 
   while ((opt = getopt(argc, argv, "n:s:t:p:")) != -1) {
@@ -89,8 +79,10 @@ int main(int argc, char **argv) {
       steps = 1 << atoi(optarg);
     else if (opt == 't')
       times = atoi(optarg);
-    else if (opt == 'p')
+    else if (opt == 'p') {
       perm = optarg[0] >= 'a' && optarg[0] <= 'c' ? optarg[0] : -1;
+      a = atoi(optarg+1);
+    }
     else
       error = true;
   }
@@ -106,7 +98,7 @@ int main(int argc, char **argv) {
     fail("Failed to allocate memory!");
 
   printf("Generate array of %d elements (%ld KiB)\n", size, size * sizeof(int) >> 10);
-  generate_permutation(array, size, perm);
+  generate_permutation(array, size, perm, a);
   flush_cache();
 
   printf("Perfom walk %d times with %d steps each.\n", times, steps);
--- list-b/raport.md
+++ list-b/raport.md
@@ -1,3 +1,9 @@
+# Procesor
+ - Core i7-3520M
+ - L1 32M - 8-way
+ - L2 256M - 8-way
+ - L3 6144M - 12-way
+
 # Zadanie 1
 Czas działania dla:
  - `./transpose -n 15 -v 0` ok 12.1 sekundy
@@ -14,4 +20,56 @@ Czas działania dla:
  - `./bsearch -n 23 -t 24 -v 1` ok 3.4 sekundy
 
 # Zadanie 4
- - 
+Każda permutacja ma jeden parametr `d`, przekazywany z linii poleceń. Do programu dodana jest flaga `-p` której argumentem jest `<podpunkt><log(d)>`. Dla przykładu `./cache -n 22 -s 23 -t 100 -p a3` oznacza, że chcemy permutację do podpunktu a, z paramatrem `d == 2 ^ 3`
+ - a) Permutacja polega na cyklicznym odczytywaniu elementów o indeksach `0, d, 2d, ...`
+    Dla parametrów `n == 22, `s == 23`, `t == 100` otrzymałem następujące wyniki
+    
+    | wartość `log(d)` | czas [s]  |
+    | -----------      | --------- |
+    | 0                | 2.30      |
+    | 1                | 2.36      |
+    | 2                | 2.44      |
+    | 3                | 2.69      |
+    | 4                | 5.06      |
+    | 5                | 11.4      |
+    | 6                | 23.1      |
+    | 10               | 60        |
+
+    Można zaobserwować, że pierwszy istotny wzrost czasu nastąpił dla `d = 16`, stąd wnioskuję, że linia cache'u ma `16  * sizeof(int)`, czyli 64 bajty długości. Wydłużenie czasu dla `d = 32` wynika z faktu, że ten model procesora ładuje po dwie linie cache'u na raz. Kolejne spowolnienia wynikają według mnie z nietrafiania w TLB.
+
+ - b) Permutacja polega na cyklicznym przeglądaniu co 16 elementu. Maksymalna wielkość tablicy dla której czas pozostaje względnie stały będzie odpowiadała wielkości poziomu cache.
+    Dla wywołania `./cache -n log(n) -s 20 -t 1000 -p b0` tablica będzie wielkości n
+    Natomiast dla wywołania `./cache log(n) -s 20 -t 1000 -p b<log(k)>` tablica będzie wielkości `n / 2 + k`
+
+    | wielkość tablicy | czas [s] |
+    | ---------------- | -------- |
+    | 4K               | 2.84     |
+    | 16K              | 2.83     |
+    | 32K              | 2.90     |
+    | 64K              | 4.81     |
+    | 128K             | 4.84     |
+    | 256K             | 4.91     |
+    | 512K             | 5.31     |
+    | 1024K            | 5.30     |
+    | 4096K            | 5.34     |
+    | 6144K            | 5.35     |
+    | 8192K            | 6.27     |
+    | 16M              | 6.30     |
+
+    Widać, że cache L1 ma rozmiar 32K, L2 ma rozmiar 256K, natomiast L3 6144K
+
+ - c) Permutacja polega na cyklicznym przeglądaniu elemtu o indeksie podzielnym przez wielkość L1
+
+    Badając iloraz `k = n / L1` poznamy rozmiar zbioru L1, wywołanie `./cache -n log(n) -s 20 -t 1000 -p c13`
+    | k     | czas [s] |
+    | ----- | -------- |
+    | 1     | 2.81     |
+    | 2     | 2.83     |
+    | 4     | 2.82     |
+    | 8     | 2.84     |
+    | 16    | 7.64     |
+      
+    Tak więc L1 ma zbiory 8 elementowe
+
+
+
